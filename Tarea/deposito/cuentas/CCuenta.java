30507a56ff39828d48a20b90b82e896106bafc26
vladyslav <vladyslav@SistemasUbuntu>
2022-03-14 02:52:16 +0100

    4. Encapsulación de atributos en getters y setters.

--- Tarea/deposito/cuentas/CCuenta.java
+++ Tarea/deposito/cuentas/CCuenta.java
@@ -14,21 +14,21 @@ public class CCuenta {
 
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
     }
 
     public double estado()
     {
-        return saldo;
+        return getSaldo();
     }
 
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
     public void retirar(double cantidad) throws Exception
@@ -37,6 +37,38 @@ public class CCuenta {
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
     }
+
+	String getNombre() {
+		return nombre;
+	}
+
+	void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	String getCuenta() {
+		return cuenta;
+	}
+
+	void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	double getSaldo() {
+		return saldo;
+	}
+
+	void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	double getTipoInterés() {
+		return tipoInterés;
+	}
+
+	void setTipoInterés(double tipoInterés) {
+		this.tipoInterés = tipoInterés;
+	}
 }
