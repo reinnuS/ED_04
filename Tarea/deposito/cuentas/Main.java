91526d3e207df25b6dfc8597232968d1231cc368
vladyslav <vladyslav@SistemasUbuntu>
2022-03-14 02:39:32 +0100

    3. Introducción del método operativa_cuenta.

--- Tarea/deposito/cuentas/Main.java
+++ Tarea/deposito/cuentas/Main.java
@@ -5,7 +5,13 @@ public class Main {
         CCuenta cuenta1;
         double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        operativa_cuenta();
+    }
+
+	private static void operativa_cuenta() {
+		CCuenta cuenta1;
+		double saldoActual;
+		cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
@@ -20,5 +26,5 @@ public class Main {
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
-    }
+	}
 }
